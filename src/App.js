3712c0fe52c3730a764d419f7a43281106c33f75
Josh G <jsgoldberg90@gmail.com>
2019-03-19 22:48:24 -0400

    add linting, and correct for lints

--- src/App.js
+++ src/App.js
@@ -1,63 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './Assets/MeruLabsLogo.png';
 import meruTattooCareLogo from './Assets/MeruTattooCareLogo.png';
 import doughDealerLogo from './Assets/DoughDealerLogo.png';
 import './App.css';
 import './Fonts/fonts.css';
 
+const goToMeru = () => {
+  window.open('https://www.merutattoocare.com', '_blank');
+};
 
-class App extends Component {
-  goToMeru = () => {
-    window.open('https://www.merutattoocare.com', '_blank');
-  };
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <a className="App-header-link" href="mailto:josh@merulabs.com">
+        contact
+      </a>
+    </header>
+    <main className="App-main">
+      <img src={ logo } className="App-logo" alt="logo" />
+      <p className="App-work-main">Our Work:</p>
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
+      <div className="App-links">
+        <div
+          onClick={ goToMeru }
+          className="App-link-container"
+          role="presentation"
+        >
+          <img src={ meruTattooCareLogo } className="App-link-logo" alt="logo" />
+          <span
+            className="App-link"
+            href="https://www.merutattoocare.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tattoo Care Products
+          </span>
+        </div>
+
+        <div className="App-link-container">
+          <img src={ doughDealerLogo } className="App-link-logo" alt="logo" />
           <a
-            className="App-header-link"
-            href="mailto:josh@merulabs.com">contact</a>
-        </header>
-        <main className="App-main">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p className="App-work-main">
-            Our Work:
-          </p>
-
-          <div className="App-links">
-
-            <div className="App-link-container">
-              <img
-                  onClick={ this.goToMeru }
-                  src={ meruTattooCareLogo }
-                  className="App-link-logo"
-                  alt="logo" />
-              <a
-                  className="App-link"
-                  href="https://www.merutattoocare.com"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                Tattoo Care Products
-              </a>
-            </div>
-
-            <div className="App-link-container">
-              <img src={doughDealerLogo} className="App-link-logo" alt="logo" />
-              <a
-                  className="App-link App-link-null"
-                  href="#ComingSoon"
-                  rel="noopener noreferrer">
-                Coming Soon
-              </a>
-            </div>
-
-          </div>
-
-        </main>
+            className="App-link App-link-null"
+            href="#ComingSoon"
+            rel="noopener noreferrer"
+          >
+            Coming Soon
+          </a>
+        </div>
       </div>
-    );
-  }
-}
+    </main>
+  </div>
+);
 
 export default App;
