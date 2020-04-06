769bce2883ec0c3aa39f6a83e4a4b52bbf82c679
Josh G <jsgoldberg90@gmail.com>
2020-04-06 14:43:53 -0400

    Updates

--- precache-manifest.0b99af5d135cf4d7338b56b922c98cb9.js
+++ precache-manifest.0b99af5d135cf4d7338b56b922c98cb9.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "3d0bbf1018536ec3621a",
-    "url": "/static/css/main.21a07c9d.chunk.css"
+    "revision": "2d93d3199e712592282f",
+    "url": "/static/css/main.c6beebcf.chunk.css"
   },
   {
-    "revision": "3d0bbf1018536ec3621a",
-    "url": "/static/js/main.720812f3.chunk.js"
+    "revision": "2d93d3199e712592282f",
+    "url": "/static/js/main.38e66a58.chunk.js"
   },
   {
     "revision": "fdfcfda2d9b1bf31db52",
@@ -28,24 +28,24 @@ self.__precacheManifest = [
     "url": "/static/media/DoughDealerLogo.2b8ac48d.png"
   },
   {
-    "revision": "c2192cf790c7fac4c4971aad0b70fce3",
-    "url": "/static/media/TTChocolates-BoldItalic.c2192cf7.woff2"
-  },
-  {
-    "revision": "724c044bbac59ddec5445a4659df9d76",
-    "url": "/static/media/TTChocolates-Regular.724c044b.woff2"
+    "revision": "ebd1813f130e3f50cadaa02cf70c8283",
+    "url": "/static/media/TTChocolates-Light.ebd1813f.woff2"
   },
   {
     "revision": "d58427cabbcb19835e9e2abbc8b8be8a",
     "url": "/static/media/TTChocolates-Italic.d58427ca.woff2"
   },
   {
-    "revision": "ebd1813f130e3f50cadaa02cf70c8283",
-    "url": "/static/media/TTChocolates-Light.ebd1813f.woff2"
+    "revision": "724c044bbac59ddec5445a4659df9d76",
+    "url": "/static/media/TTChocolates-Regular.724c044b.woff2"
   },
   {
     "revision": "6b358842b03f80666f4e7e9d77530688",
     "url": "/static/media/TTChocolates-Black.6b358842.woff2"
+  },
+  {
+    "revision": "c2192cf790c7fac4c4971aad0b70fce3",
+    "url": "/static/media/TTChocolates-BoldItalic.c2192cf7.woff2"
   },
   {
     "revision": "b267554ffc2b5f426e54a249bf87c6f7",
@@ -68,7 +68,7 @@ self.__precacheManifest = [
     "url": "/static/media/TTChocolates-Black.1a95c16b.woff"
   },
   {
-    "revision": "3a6eb3785f11c6e471f4cc7810105ceb",
+    "revision": "c34ab71b5779929bd98219e720b65615",
     "url": "/index.html"
   }
 ];
