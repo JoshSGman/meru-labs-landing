0e5328140c6fdf0c6de70461076adc6175b7f728
Josh G <jsgoldberg90@gmail.com>
2020-04-06 15:01:53 -0400

    Updates

--- precache-manifest.231e2626539b6baa26c623fd49590afa.js
+++ precache-manifest.231e2626539b6baa26c623fd49590afa.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "3d0bbf1018536ec3621a",
+    "revision": "40811f81eec05ef8c318",
     "url": "/static/css/main.21a07c9d.chunk.css"
   },
   {
-    "revision": "3d0bbf1018536ec3621a",
-    "url": "/static/js/main.720812f3.chunk.js"
+    "revision": "40811f81eec05ef8c318",
+    "url": "/static/js/main.a6463ed1.chunk.js"
   },
   {
-    "revision": "fdfcfda2d9b1bf31db52",
-    "url": "/static/js/runtime~main.c5541365.js"
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "04eafdf852d985298145",
-    "url": "/static/js/2.bddbbffd.chunk.js"
+    "revision": "9744e32435a2d8207cc7",
+    "url": "/static/js/2.1892e9e7.chunk.js"
   },
   {
     "revision": "69366185dfb648a12f6d2531d03a3cb1",
@@ -32,16 +32,16 @@ self.__precacheManifest = [
     "url": "/static/media/TTChocolates-BoldItalic.c2192cf7.woff2"
   },
   {
-    "revision": "ebd1813f130e3f50cadaa02cf70c8283",
-    "url": "/static/media/TTChocolates-Light.ebd1813f.woff2"
-  },
-  {
     "revision": "724c044bbac59ddec5445a4659df9d76",
     "url": "/static/media/TTChocolates-Regular.724c044b.woff2"
   },
   {
     "revision": "d58427cabbcb19835e9e2abbc8b8be8a",
     "url": "/static/media/TTChocolates-Italic.d58427ca.woff2"
+  },
+  {
+    "revision": "ebd1813f130e3f50cadaa02cf70c8283",
+    "url": "/static/media/TTChocolates-Light.ebd1813f.woff2"
   },
   {
     "revision": "6b358842b03f80666f4e7e9d77530688",
@@ -52,10 +52,6 @@ self.__precacheManifest = [
     "url": "/static/media/TTChocolates-BoldItalic.b267554f.woff"
   },
   {
-    "revision": "803f922c80341e93fc401d15c1caf0fe",
-    "url": "/static/media/TTChocolates-Light.803f922c.woff"
-  },
-  {
     "revision": "78fd534b6aac07b9a4e94bdf42b9617f",
     "url": "/static/media/TTChocolates-Regular.78fd534b.woff"
   },
@@ -64,11 +60,15 @@ self.__precacheManifest = [
     "url": "/static/media/TTChocolates-Italic.88ea61bb.woff"
   },
   {
+    "revision": "803f922c80341e93fc401d15c1caf0fe",
+    "url": "/static/media/TTChocolates-Light.803f922c.woff"
+  },
+  {
     "revision": "1a95c16bdbaf04553567046d175107f0",
     "url": "/static/media/TTChocolates-Black.1a95c16b.woff"
   },
   {
-    "revision": "3a6eb3785f11c6e471f4cc7810105ceb",
+    "revision": "290f491a9111b1a0dd1b03ee79460f43",
     "url": "/index.html"
   }
 ];
