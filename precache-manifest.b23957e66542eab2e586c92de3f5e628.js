f1669048c5eed0f2b51eac70fc48e766406a4a0a
Josh G <jsgoldberg90@gmail.com>
2020-04-06 14:50:14 -0400

    Updates

--- precache-manifest.b23957e66542eab2e586c92de3f5e628.js
+++ precache-manifest.b23957e66542eab2e586c92de3f5e628.js
@@ -36,36 +36,36 @@ self.__precacheManifest = [
     "url": "/static/media/TTChocolates-Light.ebd1813f.woff2"
   },
   {
-    "revision": "6b358842b03f80666f4e7e9d77530688",
-    "url": "/static/media/TTChocolates-Black.6b358842.woff2"
+    "revision": "724c044bbac59ddec5445a4659df9d76",
+    "url": "/static/media/TTChocolates-Regular.724c044b.woff2"
   },
   {
     "revision": "d58427cabbcb19835e9e2abbc8b8be8a",
     "url": "/static/media/TTChocolates-Italic.d58427ca.woff2"
   },
   {
-    "revision": "724c044bbac59ddec5445a4659df9d76",
-    "url": "/static/media/TTChocolates-Regular.724c044b.woff2"
+    "revision": "6b358842b03f80666f4e7e9d77530688",
+    "url": "/static/media/TTChocolates-Black.6b358842.woff2"
   },
   {
     "revision": "803f922c80341e93fc401d15c1caf0fe",
     "url": "/static/media/TTChocolates-Light.803f922c.woff"
   },
   {
-    "revision": "78fd534b6aac07b9a4e94bdf42b9617f",
-    "url": "/static/media/TTChocolates-Regular.78fd534b.woff"
-  },
-  {
     "revision": "b267554ffc2b5f426e54a249bf87c6f7",
     "url": "/static/media/TTChocolates-BoldItalic.b267554f.woff"
   },
   {
-    "revision": "88ea61bb3c15c1d39ca5fc50f9c3c989",
-    "url": "/static/media/TTChocolates-Italic.88ea61bb.woff"
+    "revision": "78fd534b6aac07b9a4e94bdf42b9617f",
+    "url": "/static/media/TTChocolates-Regular.78fd534b.woff"
   },
   {
     "revision": "1a95c16bdbaf04553567046d175107f0",
     "url": "/static/media/TTChocolates-Black.1a95c16b.woff"
+  },
+  {
+    "revision": "88ea61bb3c15c1d39ca5fc50f9c3c989",
+    "url": "/static/media/TTChocolates-Italic.88ea61bb.woff"
   },
   {
     "revision": "3a6eb3785f11c6e471f4cc7810105ceb",
